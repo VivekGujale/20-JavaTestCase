9d55f5e626dbc3e860ae8d78e5a5d98ebc67c965
VivekGujale <vdgujale007@.com>
2022-11-11 20:57:50 +0530

    Added logic for password validation rule3

---
+++ src/test/java/org/example/UserRegistrationUC/UserRegistrationUC7Test.java
@@ -0,0 +1,24 @@
+package org.example.UserRegistrationUC;
+
+
+import org.example.UserRegistrationUC7;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserRegistrationUC7Test {
+
+    @Test
+    public void givenPasswordAtLeastOneNumber_WhenValid_ThenTrue() {
+        boolean result = UserRegistrationUC7.isPasswordHaveNumber("Vivekg07");
+        Assert.assertTrue(result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void givenPasswordAtLeastOneNumber_WhenInvalid_ThenFalse() {
+        boolean result1 = UserRegistrationUC7.isPasswordHaveNumber("vivekg07");
+        Assert.assertFalse(result1);
+        System.out.println(result1);
+    }
+
+}
